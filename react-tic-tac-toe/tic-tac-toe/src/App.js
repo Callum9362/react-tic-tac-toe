9d763e55034477a1d3c94ef66ddbd9c2acd84d50
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-10-28 10:48:41 +1100

    Created square component

---
+++ react-tic-tac-toe/tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Square from './components/square';
+
+function App() {
+  return (
+    <div className="App">
+      <Square value="X" onClick={() => alert('X')} />
+    </div>
+  );
+}
+
+export default App;
