7fe676fe9a7bbb33d477616dd50443045a226ec1
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-01 18:00:47 +1100

    Bootstrapped project and added in a navigation bar component

---
+++ recipe-app/src/components/AppNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar app color="purple lighten-3" dark>
+    <v-toolbar-side-icon
+      class="hidden-md-and-up"
+      @click="drawer = !drawer"
+    ></v-toolbar-side-icon>
+    <v-spacer class="hidden-md-and-up"></v-spacer>
+    <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+    <v-btn color="purple lighten-3" flat class="hidden-sm-and-down">Menu</v-btn>
+    <v-spacer class="hidden-sm-and-down"></v-spacer>
+    <v-btn color="purple lighten-3" flat class="hidden-sm-and-down">SIGN IN</v-btn>
+    <v-btn color="purple lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "AppNavigation",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ recipe-app/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+
+<app-navigation> </app-navigation>
+
+</template>
+
+<script>
+import AppNavigation from '../components/AppNavigation.vue';
+
+
+export default {
+  name: "Home",
+  components: {AppNavigation}
+};
+    
+</script>
