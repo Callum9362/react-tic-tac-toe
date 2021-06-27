3e68b4d5e1d46a57a0b4efc319080fb2c82e97a1
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-06-27 10:07:18 -0700

    Created initial project with vue and tailwind

---
+++ crypto-tailwind/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar v-bind:links="['Home', 'About', 'Contact']"/>
+  <Login />
+  <Footer />
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Footer from './components/Footer'
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Login,
+    Footer
+  }
+}
+</script>
+
+<style>
+</style>
